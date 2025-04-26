34e8ed5fc2552d87079172e7edd91167ff388a44
Vu Quang Huy <vquanghuy@gmail.com>
2025-04-26 16:31:10 +0700

    Initial Commit

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by Vu Quang Huy on 26/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
