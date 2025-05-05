2cff1959ddd4b62d45045c6ca3097e728cd47854
Vu Quang Huy <vquanghuy@gmail.com>
2025-05-05 14:25:04 +0700

    chore: Correct target compile files

---
+++ Shared/Camera.cpp
@@ -0,0 +1,8 @@
+//
+//  Camera.cpp
+//  04-Camera
+//
+//  Created by Vu Quang Huy on 5/5/25.
+//
+
+#include "Camera.hpp"
---
+++ Shared/Camera.h
@@ -0,0 +1,13 @@
+//
+//  Camera.hpp
+//  04-Camera
+//
+//  Created by Vu Quang Huy on 5/5/25.
+//
+
+#ifndef Camera_hpp
+#define Camera_hpp
+
+#include <stdio.h>
+
+#endif /* Camera_hpp */
